24183ae5ad8b85b2c814b27dc7b6af2b6d2c9375
faith adline <adlinefaith@gmail.com>
2023-09-19 10:56:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 19;
+const num4 = 5;
+
+const multiply = num1 * num2; 
+const random = Math.ceil(Math.random() * 1000);
+
+const mod = num3 % num4;
+
+console.log('Mod:', mod);
+const numbersSet = [10, 15, 20];
+const max = Math.max(...numbersSet);
 
 // Write your code here
